20c39ff4c79150979339b11c072561e42cdb3e5d
ABSHARULHUDA SAYED <absharulhuda@gmail.com>
2024-08-24 18:42:23 +0530

    modified

--- lib/Common/utils.dart
+++ lib/Common/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const apiKey = "0cc69ecd1da909cf490e5ade82fea98a";
+const apiKey = " "; //own api key
 const Kbackgroundcolor = Colors.black;
 const imageUrl = "https://image.tmdb.org/t/p/w500";
 
-const barerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2M2OWVjZDFkYTkwOWNmNDkwZTVhZGU4MmZlYTk4YSIsIm5iZiI6MTcyNDA0NzMzMi4yMjIwNzQsInN1YiI6IjY0NzU4YTYyNjc0M2ZhMDEzNmVlYzFkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ky7nrz6V8K9Es7vpixwIPZ9NBpULrJWhfJgYdreSnP0";
+const barerToken = " "; //own bearer token
